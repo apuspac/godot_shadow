17edf139eb1f1fb08b382f80afff4017f1e1ab23
paulshadowsx <70123533+paulshadowsx@users.noreply.github.com>
2024-07-29 18:30:44 -0600

    Create script for enemy scene
    Makes enemy change directions when it detects collision.

---
+++ scripts/enemy.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+const SPEED = 60
+
+var direction = -1
+
+@onready var ray_cast_right = $RayCast2DRight
+@onready var ray_cast_left = $RayCast2D2Left
+@onready var animated_sprite = $AnimatedSprite2D
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = false
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = true
+	
+	position.x += direction * SPEED * delta
